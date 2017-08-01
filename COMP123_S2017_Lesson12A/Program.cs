2e635b31e12e12499ea3403aedd1ce66277f57ef
SNandalan <snanda2@my.centennialcollege.ca>
2017-08-01 15:29:24 -0400

    Created Demo application

--- COMP123_S2017_Lesson12A/Program.cs
+++ COMP123_S2017_Lesson12A/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name: Sushmita Nandalan
+ * Dtae: August 1st, 2017
+ * Description: This is a Demo application to showcase User Interface
+ * Version: 0.1 - Created the Project
+ */
+
 namespace COMP123_S2017_Lesson12A
 {
     static class Program
@@ -16,7 +23,7 @@ namespace COMP123_S2017_Lesson12A
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Demo());
         }
     }
 }
